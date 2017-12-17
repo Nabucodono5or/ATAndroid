07a82f69b3b164bd25f6f624e5795e892dfbf140
daenerys <pereirajefferson06@gmail.com>
2017-12-16 23:46:47 -0200

    preparando retrofit.

---
+++ app/src/main/java/com/example/daenerys/atandroid/SimpleService.java
@@ -0,0 +1,14 @@
+package com.example.daenerys.atandroid;
+
+import java.util.List;
+
+import retrofit2.http.GET;
+
+/**
+ * Created by daenerys on 12/16/17.
+ */
+
+public interface SimpleService {
+    
+
+}
